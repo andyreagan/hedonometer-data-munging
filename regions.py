61e3a5155a31056b9e2813531889640a325b6505
Andy Reagan <andy@andyreagan.com>
2015-02-05 02:49:45 +0000

    added timeseries

--- regions.py
+++ regions.py
@@ -18,13 +18,15 @@
 # andy reagan
 # 2015-02-04
 
+from numpy import zeros, array
+from labMTsimple.storyLab import *
 import subprocess
 import datetime
 import os
-import numpy
 import sys
 import copy
-from labMTsimple.storyLab import *
+import codecs
+import re
 
 regions = [["France","79","french",],["Germany","86","german",],["England","239","english",],["Spain","213","spanish",],["Brazil","32","portuguese",],["Mexico","145","spanish",],["South-Korea","211","korean",],["Egypt","69","arabic",],["Australia","14","english",],["New-Zealand","160","english",],["Canada","41","english",],["Canada-fr","41","french",],]
 
@@ -35,7 +37,8 @@ def processregion(region,date):
 
     # add up the previous vectors
     rest('prevvectors',date,date,region)
-    
+
+    timeseries('append',date,date,region,useStopWindow=True)
     
 def allregions(date):
     for region in regions:
@@ -55,7 +58,7 @@ def rsync(region,date):
         os.mkdir('/usr/share/nginx/data/word-vectors/{0}'.format(region[0].lower()))
         subprocess.call("rsync -avzr vacc1:/users/a/r/areagan/fun/twitter/jake/pullTweets/word-vectors/{1}-{0}-word-vector.csv /usr/share/nginx/data/word-vectors/{2}/{1}-sum.csv".format(region[0],datetime.datetime.strftime(date,'%Y-%m-%d'),region[0].lower()),shell=True)
 
-def sumfiles(start,end,array,lang,numw):
+def sumfiles(start,end,wordvec,lang,numw):
     curr = copy.copy(start)
     while curr <= end:
         # print "adding {0}".format(curr.strftime('%Y-%m-%d'))
@@ -65,14 +68,14 @@ def sumfiles(start,end,array,lang,numw):
             f.close()
             while len(tmp) > numw:
                 del(tmp[-1])
-            array=array+numpy.array(map(int,tmp))
+            wordvec=wordvec+array(map(int,tmp))
         except:
             print "could not load {0}".format(curr.strftime('%Y-%m-%d'))
             print 'could not load word-vectors/{1}/{0}-sum.csv'.format(curr.strftime('%Y-%m-%d'),lang)
             # raise 
         curr+=datetime.timedelta(days=1)
 
-    return array,curr
+    return wordvec,curr
 
 def rest(option,start,end,region,outfile='test.csv',days=[]):
     # if the option is range, pass it an outfile to write to
@@ -83,10 +86,10 @@ def rest(option,start,end,region,outfile='test.csv',days=[]):
 
         maincurr = copy.copy(start+datetime.timedelta(days=-1))
         while maincurr<end:
-            array = numpy.zeros(numw)
+            wordvec = zeros(numw)
             # added the try loop to handle failure inside the sumfiles
             # try:
-            [total,date] = sumfiles(maincurr+datetime.timedelta(days=-6),maincurr+datetime.timedelta(days=0),array,region[0].lower(),numw)
+            [total,date] = sumfiles(maincurr+datetime.timedelta(days=-6),maincurr+datetime.timedelta(days=0),wordvec,region[0].lower(),numw)
             
             # write the total into a file for date
             print "printing to {0}-prev7.csv".format(date.strftime('%Y-%m-%d'))
@@ -103,9 +106,9 @@ def rest(option,start,end,region,outfile='test.csv',days=[]):
         labMT,labMTvector,labMTwordList = emotionFileReader(stopval=0.0,fileName='labMT2'+region[2]+'.txt',returnVector=True)
         numw = len(labMTvector)
 
-        array = numpy.zeros(numw)
+        wordvec = zeros(numw)
 
-        [total,date] = sumfiles(start,end,array,lang,numw)
+        [total,date] = sumfiles(start,end,wordvec,lang,numw)
             
         # write the total into a file for date
         print "printing to {0}".format(date.strftime('%Y-%m-%d'))
@@ -122,94 +125,44 @@ def rest(option,start,end,region,outfile='test.csv',days=[]):
         labMT,labMTvector,labMTwordList = emotionFileReader(stopval=0.0,fileName='labMT2'+region[2]+'.txt',returnVector=True)
         numw = len(labMTvector)
 
-        array = numpy.zeros(numw)
+        wordvec = zeros(numw)
         for day in days:
             # this should handle the array object by reference
-            [tmp,date] = sumfiles(day,day,array,lang,numw)
-            array+=tmp
+            [tmp,date] = sumfiles(day,day,wordvec,lang,numw)
+            wordvec+=tmp
             
         # write the total into a file for date
         print "printing to {0}".format(outfile)
         f = open(outfile,'w')
         for i in xrange(numw):
-            f.write('{0:.0f}\n'.format(array[i]))
+            f.write('{0:.0f}\n'.format(wordvec[i]))
         f.close()
 
-if __name__ == '__main__':
-
-    # do the rsync
-    start = datetime.datetime(2015,2,4)
-    end = datetime.datetime.now()
-
-    loopdates(start,end)
-
-
-
-
-
-
-
-
-
-
-# take the hourly-frequency and word vectors
-# sum them to day level
-# compute happiness at hour level, day level
-# for all keywords
-#
-# USAGE: 
-# python timeseries.py append 2014-01-01 2014-06-26 english
-
-import codecs
-from labMTsimple.storyLab import *
-import datetime
-import copy
-import numpy as np
-import sys
-import re
-
-if __name__ == '__main__':
-    [year,month,day] = map(int,sys.argv[1].split('-'))
-    start = datetime.datetime(year,month,day)
-
-    [year,month,day] = map(int,sys.argv[2].split('-'))
-    end = datetime.datetime(year,month,day)
-
-    goal = sys.argv[3]
-
-    lang = sys.argv[4]
-    # lang = "english"
-
-    useStopWindow = True
-    if len(sys.argv) > 5:
-        if sys.argv[5] in ["false","False","0"]:
-            useStopWindow = False
-            print "not using a stop window"
-
-    labMT,labMTvector,labMTwordList = emotionFileReader(stopval=0.0,fileName='labMT2'+lang+'.txt',returnVector=True)
+def timeseries(option,start,end,region,useStopWindow=True):
+    labMT,labMTvector,labMTwordList = emotionFileReader(stopval=0.0,fileName='labMT2'+region[2]+'.txt',returnVector=True)
     numw = len(labMTvector)
-    if goal == "recompute":
+    if option == "recompute":
         print "opening in write mode"
-        g = codecs.open('word-vectors/'+lang+'/sumhapps.csv','w','utf8')
+        g = codecs.open('word-vectors/'+region[0].lower()+'/sumhapps.csv','w','utf8')
         g.write('date,value\n')
-        h = codecs.open('word-vectors/'+lang+'/sumfreq.csv','w','utf8')
+        h = codecs.open('word-vectors/'+region[0].lower()+'/sumfreq.csv','w','utf8')
     else:
         print "opening in append mode"        
-        g = codecs.open('word-vectors/'+lang+'/sumhapps.csv','a','utf8')
-        h = codecs.open('word-vectors/'+lang+'/sumfreq.csv','a','utf8')
+        g = codecs.open('word-vectors/'+region[0].lower()+'/sumhapps.csv','a','utf8')
+        h = codecs.open('word-vectors/'+region[0].lower()+'/sumfreq.csv','a','utf8')
     # loop over time
     currDay = copy.copy(start)
     while currDay <= end:
         # empty array
-        happsarray = np.zeros(24)
-        dayhappsarray = np.zeros(1)
-        wordarray = [np.zeros(numw) for i in xrange(24)]
-        daywordarray = np.zeros(numw)
+        happsarray = zeros(24)
+        dayhappsarray = zeros(1)
+        wordarray = [zeros(numw) for i in xrange(24)]
+        daywordarray = zeros(numw)
 
-        print 'reading word-vectors/'+lang+'/{0}'.format(currDay.strftime('%Y-%m-%d-sum.csv'))
+        print 'reading word-vectors/'+region[0].lower()+'/{0}'.format(currDay.strftime('%Y-%m-%d-sum.csv'))
         try:
-            f = codecs.open('word-vectors/'+lang+'/{0}-sum.csv'.format(currDay.strftime('%Y-%m-%d')),'r','utf8')
-            daywordarray = np.array(map(float,f.read().split('\n')[0:numw]))
+            f = codecs.open('word-vectors/'+region[0].lower()+'/{0}-sum.csv'.format(currDay.strftime('%Y-%m-%d')),'r','utf8')
+            daywordarray = array(map(float,f.read().split('\n')[0:numw]))
             f.close()
             # print daywordarray
             # print len(daywordarray)
@@ -223,8 +176,8 @@ if __name__ == '__main__':
 
     
             # write out the day happs
-            print 'writing word-vectors/{1}/{0}'.format(currDay.strftime('%Y-%m-%d-sum.csv'),lang)
-            f = codecs.open('word-vectors/{1}/{0}-sumhapps.csv'.format(currDay.strftime('%Y-%m-%d'),lang),'w','utf8')
+            print 'writing word-vectors/{1}/{0}'.format(currDay.strftime('%Y-%m-%d-sum.csv'),region[0].lower())
+            f = codecs.open('word-vectors/{1}/{0}-sumhapps.csv'.format(currDay.strftime('%Y-%m-%d'),region[0].lower()),'w','utf8')
             f.write('{0},{1}\n'.format(currDay.strftime('%Y-%m-%d'),dayhappsarray[0]))    
             f.close()
     
@@ -253,6 +206,12 @@ if __name__ == '__main__':
     g.close()
     h.close()
 
+if __name__ == '__main__':
 
+    # do the rsync
+    start = datetime.datetime(2015,2,4)
+    end = datetime.datetime.now()
+
+    loopdates(start,end)
 
 
