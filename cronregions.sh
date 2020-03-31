a863396e788088108ef148a20d4e85cd92eb8665
Andy Reagan <areagan@massmutual.com>
2020-03-31 15:12:05 -0400

    Update cronregions shell script.

--- cronregions.sh
+++ cronregions.sh
@@ -1,5 +1,4 @@
-source /home/prod/.env
-cd /usr/share/nginx/data
+source /var/lib/jenkins/hedonometer_secrets.sh
 export PYTHONPATH="$PYTHONPATH:/home/prod/app"
 export DJANGO_SETTINGS_MODULE="mysite.settings"
 python regions.py
