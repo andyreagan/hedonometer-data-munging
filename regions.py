36a8b559a9eab0f3e7da9c44e338afac1eb82f2c
Andy Reagan <areagan@massmutual.com>
2020-04-03 09:38:12 -0400

    Load models after django setup.

--- regions.py
+++ regions.py
@@ -27,11 +27,12 @@ from os.path import isdir, isfile
 
 import click
 import django
-from hedonometer.models import Timeseries
 from labMTsimple.storyLab import emotionV, shift, stopper
 from numpy import array, float, sum, zeros
 
 django.setup()
+from hedonometer.models import Timeseries  # noqa:E402 isort:skip
+
 DATA_DIR = "/usr/share/nginx/data"
 with open(os.path.join(DATA_DIR, Timeseries.objects.all()[0].directory, "stopwords.csv"), "r") as f:
     IGNORE = f.read().split("\n")
