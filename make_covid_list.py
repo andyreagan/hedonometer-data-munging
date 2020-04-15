3b16479474c25ca9a1047ac96937ba30b195a33f
Andy Reagan <areagan@massmutual.com>
2020-04-15 12:26:45 -0400

    Update script.

--- make_covid_list.py
+++ make_covid_list.py
@@ -1,3 +1,5 @@
+import json
+import os
 from pathlib import Path
 
 # these are the new words and scores
@@ -21,36 +23,28 @@ covid_words = {
     "ventilators": ("3.26", "1.97"),
 }
 
-spanish_translation = {
-    "corona": ["corona"],
-    "coronavirus": ["coronavirus"],
-    "covid": ["covid"],
-    "distancing": ["distanciamiento"],
-    "epidemic": ["epidemia"],
-    "kits": ["kits"],
-    "lockdown": ["encierro", "cierre", "confinamiento"],
-    "masks": ["mascarilla", "cubrebocas"],
-    "outbreak": ["epidemia"],
-    "pandemic": ["pandemia"],
-    "quarantine": ["cuarentena"],
-    "quarantined": ["cuarentena"],
-    "sanitizer": ["desinfectante"],
-    "self-quarantine": ["autoaislamiento"],
-    "ventilator": ["respirador"],
-    "ventilators": ["respiradores"],
-}
+all_translation = json.loads(
+    (
+        Path(os.environ.get("HOME"))
+        / Path("projects/2020/tweet_utils/data/opt_data/translations.json")
+    ).read_text()
+)
 
-covid_words = {"english": covid_words, "spanish": {}}
-translations = {"spanish": {}}
-for word, score in covid_words["english"].items():
-    if word in spanish_translation:
-        for spanish_word in spanish_translation[word]:
-            if len(spanish_word.split(' ')) == 1:
-                covid_words["spanish"][spanish_word] = score
-                translations["spanish"][spanish_word] = word
+covid_words = {"english": covid_words}
+translations = dict()
+for lang, translation in all_translation.items():
+    covid_words[lang] = dict()
+    translations[lang] = dict()
+    for word, score in covid_words["english"].items():
+        if word in translation:
+            for lang_word in translation[word]:
+                if len(lang_word.split(" ")) == 1:
+                    covid_words[lang][lang_word.lower()] = score
+                    translations[lang][lang_word.lower()] = word
 
-
-for lang in {"english", "spanish"}:
+for lang in covid_words.keys():
+    print("-" * 80)
+    print("-" * 80)
     print(lang)
     words = Path("LabMT/labMTwords-" + lang + ".csv").read_text().strip().split("\n")
     scores = Path("LabMT/labMTscores-" + lang + ".csv").read_text().strip().split("\n")
@@ -64,27 +58,52 @@ for lang in {"english", "spanish"}:
     lookup_nohashtags = {
         word: (score, stddev)
         for word, score, stddev in zip(words, scores, stddevs)
-        if word[0] not in {"#", "@"}
+        if ((word[0] not in {"#", "@"}) and (word[:3] not in {"rt@", "#-@"}) and (word[:2] != "-@"))
     }
-    len(lookup)  # 10222
-    len(lookup_nohashtags)  # 10192
-    lookup_hashtags = {
-        word: (score, lookup.get(word[1:])) for word, score in zip(words, scores) if word[0] == "#"
-    }  # included this output above
+    print(
+        "removed",
+        len(lookup),
+        "-",
+        len(lookup_nohashtags),
+        "=",
+        len(lookup) - len(lookup_nohashtags),
+        "words",
+    )
+    print(
+        "those words are:",
+        ", ".join([word for word in lookup.keys() if word not in lookup_nohashtags]),
+    )
+
+    # add the new words
+    combined_nohashtag = dict()
+    combined_nohashtag.update(lookup_nohashtags)
+    combined_nohashtag.update(covid_words[lang])
+
+    combined_nohashtag_en = dict()
+    if lang != "english":
+        combined_nohashtag_en.update(lookup_nohashtags_en)
+        combined_nohashtag_en.update(translations[lang])
+    print(
+        "with covid words, list is now",
+        len(combined_nohashtag),
+        "-",
+        len(lookup_nohashtags),
+        "=",
+        len(combined_nohashtag) - len(lookup_nohashtags),
+        "words longer",
+    )
 
     for word in covid_words[lang]:
         if word in lookup_nohashtags:
-            print("word exists already:", word, lookup_nohashtags[word], covid_words[lang][word])
-
-    # add the new words
-    combined_nohashtag = {}
-    combined_nohashtag.update(lookup_nohashtags)
-    combined_nohashtag.update(covid_words[lang])
-    len(combined_nohashtag)  # 10209
-    if lang != "english":
-        combined_nohashtag_en = {}
-        combined_nohashtag_en.update(lookup_nohashtags_en)
-        combined_nohashtag_en.update(translations[lang])
+            print(
+                "word existed already:",
+                word,
+                combined_nohashtag_en.get(word),
+                "old score/stddev:",
+                lookup_nohashtags[word],
+                "new score/stddev:",
+                combined_nohashtag[word],
+            )
 
     # write them out, first the nonhashtags then the hashtags
     words = list(combined_nohashtag.keys())
@@ -112,22 +131,23 @@ for lang in {"english", "spanish"}:
     )
     if lang != "english":
         Path("labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv").write_text("\n".join(words_en))
-    print("wc labMT/labMTwords-" + lang + "-covid.csv")
-    print("wc labMT/labMTscores-" + lang + "-covid.csv")
-    print("head labMT/labMTwords-" + lang + "-covid.csv")
-    print("head labMT/labMTscores-" + lang + "-covid.csv")
-    print("tail labMT/labMTwords-" + lang + "-covid.csv")
-    print("tail labMT/labMTscores-" + lang + "-covid.csv")
-    print("wc labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
-    print("wc labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
-    print("head labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
-    print("head labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
-    print("tail labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
-    print("tail labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
-    if lang != "english":
-        print("wc labMT/labMTwordsEn-" + lang + "-covid.csv")
-        print("head labMT/labMTwordsEn-" + lang + "-covid.csv")
-        print("tail labMT/labMTwordsEn-" + lang + "-covid.csv")
-        print("wc labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
-        print("head labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
-        print("tail labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
+    # diagnostics:
+    # print("wc labMT/labMTwords-" + lang + "-covid.csv")
+    # print("wc labMT/labMTscores-" + lang + "-covid.csv")
+    # print("head labMT/labMTwords-" + lang + "-covid.csv")
+    # print("head labMT/labMTscores-" + lang + "-covid.csv")
+    # print("tail labMT/labMTwords-" + lang + "-covid.csv")
+    # print("tail labMT/labMTscores-" + lang + "-covid.csv")
+    # print("wc labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
+    # print("wc labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
+    # print("head labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
+    # print("head labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
+    # print("tail labMT/labMTwords-" + lang + "-v2-2020-03-28.csv")
+    # print("tail labMT/labMTscores-" + lang + "-v2-2020-03-28.csv")
+    # if lang != "english":
+    #     print("wc labMT/labMTwordsEn-" + lang + "-covid.csv")
+    #     print("head labMT/labMTwordsEn-" + lang + "-covid.csv")
+    #     print("tail labMT/labMTwordsEn-" + lang + "-covid.csv")
+    #     print("wc labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
+    #     print("head labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
+    #     print("tail labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv")
