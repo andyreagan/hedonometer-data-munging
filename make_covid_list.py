934e9f07966ab8ff38598dc796884cfde1c9b7c8
Andy Reagan <areagan@massmutual.com>
2020-04-16 10:13:45 -0400

    Fix a bug on indonesian.

--- make_covid_list.py
+++ make_covid_list.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 # these are the new words and scores
-covid_words = {
+english_covid_words = {
     "corona": ("1.72", "0.97"),
     "coronavirus": ("1.34", "0.66"),
     "covid": ("1.6", "0.76"),
@@ -30,34 +30,42 @@ all_translation = json.loads(
     ).read_text()
 )
 
-covid_words = {"english": covid_words}
-translations = dict()
+covid_words = {"english": english_covid_words}
+translations = {"english": {x: x for x in english_covid_words.keys()}}
 for lang, translation in all_translation.items():
     covid_words[lang] = dict()
     translations[lang] = dict()
-    for word, score in covid_words["english"].items():
+    for word, scores in covid_words["english"].items():
         if word in translation:
             for lang_word in translation[word]:
                 if len(lang_word.split(" ")) == 1:
-                    covid_words[lang][lang_word.lower()] = score
+                    covid_words[lang][lang_word.lower()] = scores
                     translations[lang][lang_word.lower()] = word
 
-for lang in covid_words.keys():
+for lang, lang_covid_words in covid_words.items():
+    lang_translations = translations[lang]
     print("-" * 80)
     print("-" * 80)
     print(lang)
+    print(lang_covid_words)
+    print(lang_translations)
+
     words = Path("LabMT/labMTwords-" + lang + ".csv").read_text().strip().split("\n")
     scores = Path("LabMT/labMTscores-" + lang + ".csv").read_text().strip().split("\n")
     stddevs = Path("LabMT/labMTscoresStd-" + lang + ".csv").read_text().strip().split("\n")
-    if lang != "english":
+    if os.path.isfile(Path("LabMT/labMTwordsEn-" + lang + ".csv")):
         words_en = Path("LabMT/labMTwordsEn-" + lang + ".csv").read_text().strip().split("\n")
-        lookup_nohashtags_en = {
-            word: word_en for word, word_en in zip(words, words_en) if word[0] not in {"#", "@"}
-        }
+    else:
+        words_en = words
     lookup = {word: score for word, score in zip(words, scores)}
     lookup_nohashtags = {
         word: (score, stddev)
         for word, score, stddev in zip(words, scores, stddevs)
+        if ((word[0] not in {"#", "@"}) and (word[:3] not in {"rt@", "#-@"}) and (word[:2] != "-@"))
+    }
+    lookup_nohashtags_en = {
+        word: word_en
+        for word, word_en in zip(words, words_en)
         if ((word[0] not in {"#", "@"}) and (word[:3] not in {"rt@", "#-@"}) and (word[:2] != "-@"))
     }
     print(
@@ -77,12 +85,12 @@ for lang in covid_words.keys():
     # add the new words
     combined_nohashtag = dict()
     combined_nohashtag.update(lookup_nohashtags)
-    combined_nohashtag.update(covid_words[lang])
+    combined_nohashtag.update(lang_covid_words)
 
     combined_nohashtag_en = dict()
-    if lang != "english":
-        combined_nohashtag_en.update(lookup_nohashtags_en)
-        combined_nohashtag_en.update(translations[lang])
+    combined_nohashtag_en.update(lookup_nohashtags_en)
+    combined_nohashtag_en.update(lang_translations)
+
     print(
         "with covid words, list is now",
         len(combined_nohashtag),
@@ -93,7 +101,7 @@ for lang in covid_words.keys():
         "words longer",
     )
 
-    for word in covid_words[lang]:
+    for word in lang_covid_words:
         if word in lookup_nohashtags:
             print(
                 "word existed already:",
@@ -116,12 +124,9 @@ for lang in covid_words.keys():
     Path("labMT/labMTscoresStd-" + lang + "-covid.csv").write_text(
         "\n".join(map(lambda x: "{0:.2f}".format(float(x[1])), scores + scores))
     )
-    if lang != "english":
-        words_en = list(combined_nohashtag_en.values())
-        hashtags_en = list(map(lambda x: "#" + x, words_en))
-        Path("labMT/labMTwordsEn-" + lang + "-covid.csv").write_text(
-            "\n".join(words_en + hashtags_en)
-        )
+    words_en = list(combined_nohashtag_en.values())
+    hashtags_en = list(map(lambda x: "#" + x, words_en))
+    Path("labMT/labMTwordsEn-" + lang + "-covid.csv").write_text("\n".join(words_en + hashtags_en))
     Path("labMT/labMTwords-" + lang + "-v2-2020-03-28.csv").write_text("\n".join(words))
     Path("labMT/labMTscores-" + lang + "-v2-2020-03-28.csv").write_text(
         "\n".join(map(lambda x: "{0:.2f}".format(float(x[0])), scores))
@@ -129,8 +134,7 @@ for lang in covid_words.keys():
     Path("labMT/labMTscoresStd-" + lang + "-v2-2020-03-28.csv").write_text(
         "\n".join(map(lambda x: "{0:.2f}".format(float(x[1])), scores))
     )
-    if lang != "english":
-        Path("labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv").write_text("\n".join(words_en))
+    Path("labMT/labMTwordsEn-" + lang + "-v2-2020-03-28.csv").write_text("\n".join(words_en))
     # diagnostics:
     # print("wc labMT/labMTwords-" + lang + "-covid.csv")
     # print("wc labMT/labMTscores-" + lang + "-covid.csv")
